1e914ec61d46272d64f0cd8341ee54d0462f5144
JoanR17 <joanr.m.1999@gmail.com>
2022-03-23 12:46:22 +0100

    Commit

--- src/main/java/ProyectoFinal/ReservesMenjador/dao/IAlergiaDAO.java
+++
@@ -1,9 +0,0 @@
-package ProyectoFinal.ReservesMenjador.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ProyectoFinal.ReservesMenjador.dto.Alergia;
-
-public interface IAlergiaDAO extends JpaRepository<Alergia, Integer>{
-
-}
