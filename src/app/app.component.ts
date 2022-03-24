e77b74d575a1d7041ad53c0d742caca0ae6f542c
JoanR17 <joanr.m.1999@gmail.com>
2022-03-24 09:44:32 +0100

    Commit 1 navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto-reserva-menjador';
+  private rol: string | null = '';
+  isLoggedIn = false;
+  showAdminBoard = false;
+  username?: string | null;
+
+  constructor () { }
+
+  ngOnInit(): void
+  {
+    this.isLoggedIn = !!window.sessionStorage.getItem("auth-token");
+
+    if(this.isLoggedIn)
+    {
+      this.username = window.sessionStorage.getItem("auth-username");
+      this.rol = window.sessionStorage.getItem("auth-rol");
+
+      this.showAdminBoard = (this.rol == 'admin');
+    }
+  }
+
+  logout(): void
+  {
+    window.sessionStorage.clear();
+    window.location.reload();
+  }
 }
