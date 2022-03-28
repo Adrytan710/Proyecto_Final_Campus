adb11d8e46a89a54f5b43723977a4802bfadfd2f
JoanR17 <joanr.m.1999@gmail.com>
2022-03-28 13:54:28 +0200

    Commit Orden

--- src/app/orden/orden.component.ts
+++ src/app/orden/orden.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiRestService } from '../_servicios/api-rest.service';
-import {FormBuilder, FormGroup, FormControl, FormArray} from '@angular/forms';
+import { UsersService } from '../_servicios/auth/users.service';
 
 @Component({
   selector: 'app-orden',
@@ -11,13 +11,17 @@ export class OrdenComponent implements OnInit {
 
   platos: any = null;
   peticionPlatos = false;
-  form: FormGroup;
+  formCheckbox: any;
+  formContador = {
+    "Entrante": 0,
+    "Primer_Plato": 0,
+    "Segundo_Plato": 0,
+    "Postre": 0
+  };
+  platosEnviar: Array<any> = [];
+  peticionEnviada = false;
 
-  constructor(private api: ApiRestService, private fb: FormBuilder) {
-    this.form = fb.group({
-      platosSelecteds:  new FormArray([])
-    });
-  }
+  constructor(private api: ApiRestService, private apiUsuario: UsersService) {}
 
   ngOnInit(): void {
     this.getPlatos();
@@ -29,19 +33,124 @@ export class OrdenComponent implements OnInit {
       data => {
         this.peticionPlatos = true;
         this.platos = data;
+
+        this.formCheckbox = [];
+        for (const plato of this.platos)
+        {
+          this.formCheckbox.push(false);
+        }
       }
     )
   }
 
-  onCheckboxChange(event: any) {
-    const platosSelecteds = (this.form.value.platosSelecteds as FormArray);
-    if (event.target.checked) {
-      platosSelecteds.push(new FormControl(event.target.value));
-    } else {
-      const index = platosSelecteds.controls
-      .findIndex(x => x.value === event.target.value);
-      platosSelecteds.removeAt(index);
+  addPlato(plato: any)
+  {
+    const id = plato.id;
+
+    if(this.formCheckbox[id-1])
+    {
+      this.platosEnviar.push(plato);
+
+      this.addSelecion(plato.categoria.nombre);
+    }
+    else
+    {
+      let array = [];
+      for (const platoArray of this.platosEnviar)
+      {
+        if(platoArray.id != id)
+        {
+          array.push(plato);
+        }
+      }
+      this.platosEnviar = array;
+      this.quitarSelecion(plato.categoria.nombre);
     }
   }
 
+  addSelecion(categoria: string)
+  {
+    switch (categoria)
+    {
+      case "Entrante":
+          this.formContador.Entrante++;
+        break;
+      case "Primer plato":
+          this.formContador.Primer_Plato++;
+        break;
+      case "Segundo plato":
+          this.formContador.Segundo_Plato++;
+        break;
+      case "Postre":
+          this.formContador.Postre++;
+        break;
+    }
+  }
+
+  quitarSelecion(categoria: string)
+  {
+    switch (categoria)
+    {
+      case "Entrante":
+          this.formContador.Entrante--;
+        break;
+      case "Primer plato":
+          this.formContador.Primer_Plato--;
+        break;
+      case "Segundo plato":
+          this.formContador.Segundo_Plato--;
+        break;
+      case "Postre":
+          this.formContador.Postre--;
+        break;
+    }
+  }
+
+  enviarDatos()
+  {
+    const data = {
+      fecha: new Date(),
+      usuario: ''
+    };
+
+    let username: any = window.sessionStorage.getItem('auth-username');
+
+    this.apiUsuario.getUsuario(username)
+    .subscribe(
+      res => {
+        data.usuario = res;
+
+        this.api.agregaElementoOrden(data)
+        .subscribe(
+          rest => {
+            this.api.getListaOrden().subscribe(
+              rest2 => {
+                let ordenes: Array<any> | any  = rest2;
+                let orden = ordenes[ordenes.length-1];
+
+                for (const plato of this.platosEnviar)
+                {
+                  this.api.agregaElementoPeticion({orden, plato})
+                  .subscribe(
+                    resultado => {
+                      this.peticionEnviada = true;
+                    },
+                    error => {
+                      console.log(error.message);
+                    }
+                  );
+                }
+              },
+              error => {
+                console.log(error.message);
+              }
+            );
+          },
+          error => {
+            console.log(error.message);
+          }
+        );
+      }
+    );
+  }
 }
