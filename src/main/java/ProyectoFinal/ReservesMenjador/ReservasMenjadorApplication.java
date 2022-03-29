26bf31c5a02544a68ca10404f23f032b2177e611
JoanR17 <joanr.m.1999@gmail.com>
2022-03-29 23:51:50 +0200

    Cors Filter

--- src/main/java/ProyectoFinal/ReservesMenjador/ReservasMenjadorApplication.java
+++ src/main/java/ProyectoFinal/ReservesMenjador/ReservasMenjadorApplication.java
@@ -2,12 +2,27 @@ package ProyectoFinal.ReservesMenjador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
 @SpringBootApplication
 public class ReservasMenjadorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservasMenjadorApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 }
--- src/main/java/ProyectoFinal/ReservesMenjador/security/WebSecurity.java
+++ src/main/java/ProyectoFinal/ReservesMenjador/security/WebSecurity.java
@@ -2,6 +2,8 @@ package ProyectoFinal.ReservesMenjador.security;
 
 import static ProyectoFinal.ReservesMenjador.security.Constants.LOGIN_URL;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -59,8 +61,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        CorsConfiguration configuration = new CorsConfiguration();
+        //configuration.setAllowedOrigins(Arrays.asList("main.d2ckn95sow20us.amplifyapp.com/"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues().combine(configuration));
 		return source;
 	}
 }
